ecefe2173ddb3eca819e0d1f3c9e1b478951efbd
Spiff Azeta <spiffazeta@yahoo.com>
2023-09-23 18:25:04 +0100

    updated output

--- output.tf
+++ output.tf
@@ -1,14 +1,18 @@
 output "connection_strings" {
-  value = mongodbatlas_cluster.my_cluster.connection_strings.0.standard_srv
-  #value = mongodbatlas_cluster.my_cluster.connection_strings.0.standard
+  value = module.mongodb_atlas.connection_strings
 }
 
 output "db_user_password" {
   description = "Database password. Do not share!"
   sensitive   = true
-  value       = random_password.user_password.result
+  value       = module.mongodb_atlas.db_user_password
 }
 
 output "mongo_vpc_id" {
-  value = mongodbatlas_network_container.mongo_container.vpc_id
+  value = module.mongodb_atlas.mongo_vpc_id
+}
+
+output "datadog_integration_id" {
+  description = "The ID of the Datadog integration"
+  value       = var.enable_datadog_integration ? module.mongodb_atlas[0].id : null
 }
