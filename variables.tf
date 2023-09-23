f3b9e37db9c1a2c403c6a59149075f8ec4efb273
Spiff Azeta <spiffazeta@yahoo.com>
2023-09-23 17:40:46 +0100

    added variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,38 @@
+# access keys
+variable "mongodb_atlas_api_pub_key" {
+  type = string
+}
+
+variable "mongodb_atlas_api_pri_key" {
+  type = string
+}
+
+variable "mongodb_atlas_org_id" {
+  type = string
+}
+
+variable "aws_account_id" {
+  type = string
+}
+
+variable "aws_access_key" {
+  type = string
+}
+
+variable "aws_secret_key" {
+  type = string
+}
+
+
+# project variables
 variable "projectName" {
   description = "The atlas project name to be created"
   default     = "my_project"
+}
+
+variable "mongodb_atlas_accesslistip" {
+  type        = string
+  description = "IP addresses/ cidr ranges allowed to access the database"
 }
 
 variable "default_tags" {
@@ -11,6 +43,13 @@ variable "default_tags" {
   }
 }
 
+# Enable or disable network peering
+variable "enable_network_peering" {
+  description = "Set to true to enable network peering with AWS resources."
+  default     = false
+}
+
+# AWS Variables (if you use VPC peering)
 variable "aws_region" {
   type        = string
   description = "Region AWS resources would be deployed in"
@@ -27,4 +66,14 @@ variable "datadog_region" {
   type        = string
   description = "Datadog region"
   default     = "US"
+}
+
+# Enable or disable Datadog integration
+variable "enable_datadog_integration" {
+  description = "Set to true to enable Datadog integration."
+  default     = false
+}
+
+variable "datadog_api_key" {
+  type = string
 }
